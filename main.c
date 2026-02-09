227d0a76436594d41eae9007616d9e9aff454df8
vicyel ugueto <dvicyel@gmail.com>
2026-02-09 10:03:39 -0400

    agregar detalles

--- main.c
+++ main.c
@@ -1,4 +1,10 @@
 int main (void)
 {
     printf("Hola Mundo");
+   // comandos varios:
+  //  git clone <repo> clonar repositorio 
+  // git fetch descargar la informacion
+  // git pull origin descargada la informacio mver al ultimo commit
+  // git commit -am "mensaje" actualizar 
+  // git push origin main empujar cambio
 }

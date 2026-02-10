393b8b690f967785d3d225004e0c5a6364f0960c
vicyel ugueto <dvicyel@gmail.com>
2026-02-10 00:23:32 -0400

    cambios 2.0

--- main.c
+++ main.c
@@ -4,12 +4,13 @@ int main (void)
    // comandos varios:
   //  git clone <repo> (clonar repositorio) git clone git@github.com:vicyelug/proyectoejemplo.git 
   // git fetch (descargar la informacion) // descarga, pero no mezcla
-  // git pull origin (descargada la informacion) mover al ultimo commit
+  // git pull origin (instalar la informacion) mover al ultimo commit
   // git commit -am "mensaje" (actualizar) git commit -am "doc 1"
   // git push origin main (empujar cambio) git push -u origin main
   // git status 
   // git checkout main (ir a)
   // git branch Dev1 (crear rama) 
   // git checkout -B Dev2 (crear rama y guardar)
+  // git reset (resetear codigo) 
 
 }
